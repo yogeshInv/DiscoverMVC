6c99f41ca74831d75c41749c5f489a4bc8d72bd4
yogesh <yogesh.hr@happiestminds.com>
2014-11-05 15:37:05 +0530

    Added SampleController

---
+++ SampleMVC/Controllers/SampleFilterController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMVC.Controllers
+{
+    public class SampleFilterController : Controller
+    {
+        // GET: SampleFilter
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
